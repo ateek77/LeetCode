9092bba3561280a8c3c0e735f87ca872cf92ca44
Mohd Ateek <58817663+ateek77@users.noreply.github.com>
2018-06-15 12:23:56 +0000

    Sync LeetCode submission - Jump Game II (cpp)

---
+++ LeetCode/problems/jump_game_ii/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+     int mx;
+    int jumpDP(vector<int> arr,int n )
+    {
+        int DP[n]={0};
+
+        for(int i=n-2;i>=0;--i)
+        {
+            int mn =INT_MAX;
+            if(!arr[i])
+                DP[i]=INT_MAX;
+            else if(arr[i]==arr[i+1]+1 && i!=n-2)
+               DP[i] = DP[i+1];
+            else
+               DP[i]  =(mn=*min_element(DP+i+1,((i+arr[i]>=n) ? (DP+n):(DP+i+arr[i]+1))))==INT_MAX ? INT_MAX :mn+1;
+        }
+        return DP[0];
+    }
+    int jump(vector<int>& nums) {
+        mx=INT_MAX;
+        mx=jumpDP(nums,nums.size());
+        if(mx==INT_MAX)
+            return -1;
+        return mx;
+    }
+};
