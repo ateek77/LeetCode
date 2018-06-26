db80250e24d5ac0c9a7b4a9a191dfd0fbc89ea8e
Mohd Ateek <58817663+ateek77@users.noreply.github.com>
2018-06-26 06:41:02 +0000

    Sync LeetCode submission - Swap Nodes in Pairs (cpp)

---
+++ LeetCode/problems/swap_nodes_in_pairs/solution.cpp
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(!head || !head->next)
+            return head;
+        swap(head->val,head->next->val);
+        swapPairs(head->next->next);
+        return head;
+    }
+};
