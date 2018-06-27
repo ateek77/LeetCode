859a07e2b5266f878852123427416fa5ec649887
Mohd Ateek <58817663+ateek77@users.noreply.github.com>
2018-06-27 17:19:48 +0000

    Sync LeetCode submission - Count Complete Tree Nodes (cpp)

--- LeetCode/problems/count_complete_tree_nodes/solution.cpp
+++ LeetCode/problems/count_complete_tree_nodes/solution.cpp
@@ -7,6 +7,12 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+static int x=[]()
+{
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
 class Solution {
 public:
    int countNodes(TreeNode* root) {
