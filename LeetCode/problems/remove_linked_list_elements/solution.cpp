2b4de39b8fb5ad3bce65fcec64fc6ea53222f9dc
Mohd Ateek <58817663+ateek77@users.noreply.github.com>
2018-06-28 08:25:42 +0000

    Sync LeetCode submission - Remove Linked List Elements (cpp)

--- LeetCode/problems/remove_linked_list_elements/solution.cpp
+++ LeetCode/problems/remove_linked_list_elements/solution.cpp
@@ -10,30 +10,10 @@ class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
         if(!head)
-                return head; 
-        ListNode *hd = head,*pre=NULL;
-        int flg=1;
-        while(hd )
-        {
-            flg=1;
-            if(hd->val == val)
-            {
-                if(!pre)
-                {
-                    head = hd->next;
-                }
-                else
-                {
-                    pre->next=hd->next;
-                    
-                }
-                flg=0;
- //               hd->next = hd->next->next; 
-            }
-            if(flg)
-               pre=hd;
-            hd=hd->next;            
-        }
+            return NULL;
+        if(head->val==val)
+            return removeElements(head->next,val);
+        head->next=removeElements(head->next,val);
         return head;
     }
 };
