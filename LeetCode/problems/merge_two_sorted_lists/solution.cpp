fd4a2bfd2d62af5c94947bb653c90fbcaf44e96b
Mohd Ateek <58817663+ateek77@users.noreply.github.com>
2022-06-02 16:46:08 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (cpp)

--- LeetCode/problems/merge_two_sorted_lists/solution.cpp
+++ LeetCode/problems/merge_two_sorted_lists/solution.cpp
@@ -3,28 +3,40 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if(l1 && l2)
-        {
-            
-                if(l1->val > l2->val){
-                    l2->next = mergeTwoLists(l1,l2->next);  
-                    return l2;
-                } 
-                else
-                {
-                    l1->next = mergeTwoLists(l1->next,l2);  
-                    return l1;
-                }
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(!list1 || !list2){
+            return list1?list1:list2;
         }
-        else if(l1)
-            return l1;
-        else
-            return l2;
+        
+        ListNode* p1,*p2,*tmp,*ans=NULL;
+        p1=list1;p2=list2;
+        
+        while(p1 && p2){
+            if(p1->val<=p2->val){
+                tmp=p1;
+                p1=p1->next;
+            }else{
+                tmp=p2;
+                p2=p2->next;                
+            }
+            if(ans)
+                ans->next=tmp;     
+            ans=tmp;
+        }
+        if(p1){
+            ans->next= p1;
+        }else if(p2)
+            ans->next=p2;
+        
+        if(list1->val<=list2->val)
+            return list1;
+        return list2;
     }
 };
