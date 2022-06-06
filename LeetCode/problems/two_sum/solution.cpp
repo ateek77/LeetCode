deb19a8e7482f39071f12bee6ec368be176b2870
Mohd Ateek <58817663+ateek77@users.noreply.github.com>
2022-06-06 15:53:31 +0000

    Sync LeetCode submission - Two Sum (cpp)

--- LeetCode/problems/two_sum/solution.cpp
+++ LeetCode/problems/two_sum/solution.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nm ,int target) {
-      //  sort(nm.begin(),nm.end());
-        vector<int> v;
-        unordered_map<int,int> s;
-     //   std::ios::sync_with_stdio(false);
-    //    cin.tie(NULL);
-        for(int i=0;i<nm.size();++i)
-        {
-            auto it =s.find(target-nm[i]);
-            if(it!=s.end())
-            {
-                v.push_back(it->second);
-                v.push_back(i);
-                return v;
-            }
-            else
-                s[nm[i]]=i;
-                
-        }         
-        return v;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        //sort(nums.begin(),nums.end());
+        // int st,en;
+        // en=nums.size()-1;
+        // st=0;
+        // while(st<en){
+        //     if(nums[en] + nums[st] == target )
+        //         return {st,en};
+        //     if(nums[en] + nums[st] > target ){
+        //         en--;
+        //     }else{
+        //         st++;
+        //     }
+        // }
+        // return {};
+        
+//         int n=nums.size();
+//         for(int i=0;i<n;++i){
+            
+//             for(int j=i+1;j<n;++j){
+//                 if(target-nums[i]==nums[j])
+//                     return {i,j};
+//             }
+//         }
+//         return {};
+        
+        int n=nums.size();
+        unordered_map<int,int> mp;
+        for(int i=0;i<n;++i){
+            if(mp.find(target-nums[i])!=mp.end()){
+                return {mp[target-nums[i]],i};
+            }else
+                mp[nums[i]]=i;
+        }
+        return {};
     }
 };
