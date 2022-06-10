59704c8572c5976a3307fab997830f716200f21f
Mohd Ateek <58817663+ateek77@users.noreply.github.com>
2022-06-10 03:59:00 +0000

    Sync LeetCode submission - Top K Frequent Elements (cpp)

--- LeetCode/problems/top_k_frequent_elements/solution.cpp
+++ LeetCode/problems/top_k_frequent_elements/solution.cpp
@@ -1,20 +1,34 @@
+
 class Solution {
 public:
+
+    struct compe{
+        bool operator()(const pair<int,int>  &a,const pair<int,int> &b){
+            return a.first>b.first;
+        }
+    };
     vector<int> topKFrequent(vector<int>& nums, int k) {
         map<int ,int> mp;
         for(auto it :  nums){
             mp[it]++;
         }
-        multimap<int ,int> mmp;
+        priority_queue<pair<int ,int>,vector<pair<int,int>>,compe> pq;
         
         for(auto it:mp){
-            mmp.insert({it.second,it.first});
+            if(pq.size()<k){            
+                pq.push(make_pair(it.second,it.first));
+            }else if(pq.top().first < it.second ){
+                 pq.pop();
+                pq.push(make_pair(it.second,it.first));
+            }
+            
         }
         vector<int> res;
         
-        for(auto it=mmp.rbegin();it!=mmp.rend() && k--;++it){
-            res.push_back(it->second);
-            
+        while(!pq.empty()){        
+            cout<<pq.top().first<<" "<<pq.top().second<<" \n";
+            res.push_back(pq.top().second);
+            pq.pop();
         }
         return res;
     }
