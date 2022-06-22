5a85566cb0415ef8a5c14ba0b8b1e7404215c51a
Mohd Ateek <58817663+ateek77@users.noreply.github.com>
2022-06-22 16:39:59 +0000

    Sync LeetCode submission - Invert Binary Tree (cpp)

--- LeetCode/problems/invert_binary_tree/solution.cpp
+++ LeetCode/problems/invert_binary_tree/solution.cpp
@@ -4,18 +4,21 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        if(!root)
-            return NULL;
-        swap(root->left,root->right);
-        
+        if(!root)   return root;
         invertTree(root->left);
-        invertTree(root->right);        
+        invertTree(root->right);
+        TreeNode *temp = root->right;
+        root->right = root->left;        
+        root->left=temp;
         return root;
+        
     }
 };
