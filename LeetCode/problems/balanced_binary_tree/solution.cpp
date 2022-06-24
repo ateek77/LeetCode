6d3056bfcaf91e4757f24966eb906ed70df282dc
Mohd Ateek <58817663+ateek77@users.noreply.github.com>
2022-06-24 04:20:02 +0000

    Sync LeetCode submission - Balanced Binary Tree (cpp)

--- LeetCode/problems/balanced_binary_tree/solution.cpp
+++ LeetCode/problems/balanced_binary_tree/solution.cpp
@@ -4,25 +4,26 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int check(TreeNode *root,int &val)
-    {
-        if(!root || !val)
-            return 0;
-        int l =check(root->left,val);
-        int r =check(root->right,val);
-        
-        if(abs(l-r)>1)
-            val=0;
-        return max(l,r)+1;
+    int hight(TreeNode* root){
+        if(!root) return 0;
+        return 1+max(hight(root->left),hight(root->right));
     }
     bool isBalanced(TreeNode* root) {
-        int chk=1;
-        check(root,chk);
-        return chk;
+        if(!root) return 1;
+        if(!isBalanced(root->left) || !isBalanced(root->right))
+            return 0;
+        int leftHight,rightHight;
+        leftHight=hight(root->left);
+        rightHight=hight(root->right);
+        if(abs(leftHight-rightHight)>1)return 0;
+        return 1;
+        
     }
 };
