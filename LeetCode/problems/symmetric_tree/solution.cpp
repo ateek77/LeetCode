ff7ed4f922b1aab83545df7c2cf1ceedb71e13ce
Mohd Ateek <58817663+ateek77@users.noreply.github.com>
2022-06-26 10:31:18 +0000

    Sync LeetCode submission - Symmetric Tree (cpp)

--- LeetCode/problems/symmetric_tree/solution.cpp
+++ LeetCode/problems/symmetric_tree/solution.cpp
@@ -4,22 +4,20 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-   bool check(TreeNode* r1,TreeNode* r2)
-    {
-        if(!r1 && !r2)
-            return 1;
-        else if(r1 && !r2 || r2 && !r1 || r1->val !=r2->val )
-            return 0;
-        return check(r1->left ,r2->right) && check(r1->right ,r2->left);
+    bool checkEqual(TreeNode *first,TreeNode *second){
+        if(!first && !second)   return 1;
+        if(!first || !second)   return 0;
+        return (first->val == second->val) && checkEqual(first->left ,second->right) && checkEqual(first->right,second->left);
     }
     bool isSymmetric(TreeNode* root) {
-        if(!root)
-            return 1;
-        return check(root->left,root->right);  
+        if(!root)   return 1;
+        return checkEqual(root->left,root->right);
     }
 };
