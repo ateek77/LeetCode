3611d65125775f555d20e4d705152ce49421e9fe
Mohd Ateek <58817663+ateek77@users.noreply.github.com>
2022-06-30 04:24:29 +0000

    Sync LeetCode submission - Flatten Binary Tree to Linked List (cpp)

--- LeetCode/problems/flatten_binary_tree_to_linked_list/solution.cpp
+++ LeetCode/problems/flatten_binary_tree_to_linked_list/solution.cpp
@@ -4,23 +4,27 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
+    void convertBSTtoLL(TreeNode* root){
+        if(!root)   return ;
+        TreeNode* left =root->left;
+        
+        while(left && left->right)
+            left=left->right;
+        if(left){
+            left->right=root->right;
+            root->right =root->left;            
+            root->left=NULL;
+        }        
+        convertBSTtoLL(root->right);
+    }
     void flatten(TreeNode* root) {
-        if(!root)
-            return ;
-        if(root->left)
-        {
-            TreeNode *tp=root->left;
-            while(tp->right)
-                    tp=tp->right;
-            tp->right = root->right;
-            root->right = root->left;
-            root->left = NULL;
-        }
-        flatten(root->right);        
+        convertBSTtoLL(root);
     }
 };
