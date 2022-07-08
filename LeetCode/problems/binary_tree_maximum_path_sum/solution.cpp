6aafea7c7e5b68d1db557963c7d8536e057c861a
Mohd Ateek <58817663+ateek77@users.noreply.github.com>
2022-07-08 19:08:14 +0000

    Sync LeetCode submission - Binary Tree Maximum Path Sum (cpp)

--- LeetCode/problems/binary_tree_maximum_path_sum/solution.cpp
+++ LeetCode/problems/binary_tree_maximum_path_sum/solution.cpp
@@ -4,23 +4,30 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int MP(TreeNode *rt,int &m)
-    {
-        if(!rt)
-            return 0;
-        int l = max(0,MP(rt->left,m));
-        int r= max(0,MP(rt->right ,m));
-        m =max(m,l+r+rt->val);
-        return rt->val+max(l,r);
+    int max_sum=INT_MIN;    
+    int maxSum(TreeNode *root){
+        if(!root)   return 0;
+        int left_sum = maxSum(root->left);
+        int right_sum = maxSum(root->right);
+        int current_max = INT_MIN;
+        current_max = max(current_max,root->val);
+        current_max = max(current_max,root->val+left_sum);
+        current_max = max(current_max,root->val+right_sum);
+        max_sum = max(current_max,max_sum);
+        max_sum = max(max_sum,left_sum + root->val + right_sum);
+        
+        return current_max;
     }
     int maxPathSum(TreeNode* root) {
-        int  m=INT_MIN;
-        MP(root,m);
-        return m;
+        maxSum(root);
+        return max_sum;
     }
+    
 };
