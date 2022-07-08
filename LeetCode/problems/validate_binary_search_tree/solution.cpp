4a42e12f6d9fb1b79e84645102e64e867ae1ab43
Mohd Ateek <58817663+ateek77@users.noreply.github.com>
2022-07-08 17:43:02 +0000

    Sync LeetCode submission - Validate Binary Search Tree (cpp)

--- LeetCode/problems/validate_binary_search_tree/solution.cpp
+++ LeetCode/problems/validate_binary_search_tree/solution.cpp
@@ -4,36 +4,38 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-     bool isValidBST(TreeNode* root) {
-
-        return f(root, LONG_MIN, LONG_MAX);
-    }
-    
-     bool f(TreeNode* root,long minv, long maxv) {
-        //std::cout<<root->val<<" "<<minv<<" " << maxv<<std::endl;
-
-        if (!root)
-            return true;
-         if (root->val <= minv)
-             return false;
-         if (root->val >= maxv)
-             return false;
-
-        if (root->left && !f(root->left, minv , root->val)) {
-            
+    bool check_validitiy(TreeNode *root,TreeNode *&prevNode){
+        if(!root)   return true;
+        if(!check_validitiy(root->left,prevNode))
             return false;
-        }
-            
-            
-       if (root->right && !f(root->right,root->val, maxv)) {
+        if((prevNode && prevNode->val >= root->val))
+            return false;
+        prevNode = root;
+        if(!check_validitiy(root->right,prevNode))
            return false;
-       }
-        return true;
+        return true;              
     }
-  
+    bool isValidBST(TreeNode* root) {
+        if(!root)   return true;
+//         bool check=1;
+        
+        
+//         if((root->left && root->left->val >=root->val))
+//             return false;
+        
+//         if(root->right && root->right->val<=root->val)
+//             return false;
+        
+//         if(isValidBST(root->left) && isValidBST(root->right))
+//             return true;
+        TreeNode *ref=NULL;
+        return check_validitiy(root,ref);
+    }
 };
