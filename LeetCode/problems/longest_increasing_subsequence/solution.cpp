b72eac77b9d16ddf1db9c943910ff6b587119d2f
Mohd Ateek <58817663+ateek77@users.noreply.github.com>
2022-07-12 03:23:06 +0000

    Sync LeetCode submission - Longest Increasing Subsequence (cpp)

--- LeetCode/problems/longest_increasing_subsequence/solution.cpp
+++ LeetCode/problems/longest_increasing_subsequence/solution.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n= nums.size();
-        if(n<1)
-            return 0;
-        int i,j,mx;
-        vector<int> DP(1);
-        DP[0]=INT_MIN;
-        for(i=0;i<n;++i)
-        {
-            if(nums[i]>DP[0])
-                DP.insert(DP.begin(),nums[i]);
-            else
-            {
-                j=0;
-                while(DP[j]>=nums[i])
-                    ++j;
-                DP[j-1] = nums[i];
-            }
+        int n = nums.size();
+        if(n<2)
+            return n;
+        vector<int> store_result(1,0);
+        store_result[0]=nums[0];
+        int len=1;
+        for(int i=1;i<n;++i){
             
-        } 
-        // for(auto it:DP)
-        //     cout<<it<<" ";
-        // cout<<"\n";
-        return DP.size()-1;
+            if(store_result[len-1]<nums[i])
+            {
+                store_result.push_back(nums[i]);
+                ++len;
+            }else{
+                int k=0;
+                while(k<store_result.size() && store_result[k]<nums[i])
+                    ++k;
+                if(k==store_result.size())
+                    store_result[k-1] = nums[i];
+                else
+                    store_result[k] = nums[i];
+            }
+            // for(auto it:store_result){
+            //     cout<<" "<<it<<" ";
+            // }
+            // cout<<"\n";
+        }
+        return len;
     }
 };
+
+//[0,1,0,3,2,3]
+//[1,2,2,3,3,4]
+//[3,2,2,1,2,1]
